0e1b19d64abe68032a9c1fda848f86e7290aa8ea
thisisishara <thisismaduishara@gmail.com>
2021-03-31 09:44:14 +0530

    Initial commit of MarketplaceService after name refactoring.

---
+++ MarketplaceService/src/com/gadgetbadget/marketplace/Test.java
@@ -0,0 +1,16 @@
+package com.gadgetbadget.marketplace;	
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/Test")
+public class Test {
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "Marketplace is up and running.";
+	}
+}
