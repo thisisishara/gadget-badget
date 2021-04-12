13fccadb1844dac01e91fc818ec26a1742bc22e5
dinushiTJ <dinushitj@gmail.com>
2021-04-12 15:37:36 +0530

    Payment: Insert Payment method created

--- PaymentService/src/com/gadgetbadget/payment/model/Payment.java
+++ PaymentService/src/com/gadgetbadget/payment/model/Payment.java
@@ -1,6 +1,7 @@
 package com.gadgetbadget.payment.model;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -9,7 +10,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 public class Payment extends DBHandler{
-	
+
 	//Read Payments
 	public JsonObject readPayment() {
 		JsonObject result = null;
@@ -64,6 +65,48 @@ public class Payment extends DBHandler{
 		}
 		return result;
 	}
+
+	//Insert Payment
+	public JsonObject insertPayment(String consumer_id, String product_id, float payment_amount) {
+		JsonObject result = null;
+		try {
+			Connection conn = getConnection();
+			if (conn == null) {
+				result = new JsonObject();
+				result.addProperty("STATUS", "ERROR");
+				result.addProperty("MESSAGE", "Operation has been terminated due to a database connectivity issue.");
+				return result; 
+			}
+
+			String query = "INSERT INTO `payment`(`consumer_id`, `product_id`, `payment_amount`,) VALUES(?,?,?);";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+
+			preparedStmt.setString(1, consumer_id);
+			preparedStmt.setString(2, product_id);
+			preparedStmt.setFloat(3, payment_amount);
+
+			int status = preparedStmt.executeUpdate();
+			conn.close();
+
+			result = new JsonObject();
+
+			if(status > 0) {
+				result.addProperty("STATUS", "SUCCESSFUL");
+				result.addProperty("MESSAGE", "Payment " + payment_amount + " made successfully by customer "+ consumer_id+".");
+			} else {
+				result.addProperty("STATUS", "UNSUCCESSFUL");
+				result.addProperty("MESSAGE", "Unable to Make the Payment " + payment_amount+" by customer "+ consumer_id+".");
+			}
+		}
+		catch (Exception ex) {
+			result = new JsonObject();
+			result.addProperty("STATUS", "EXCEPTION");
+			result.addProperty("MESSAGE", "Error occurred while making Payment " +payment_amount+ " of customer "+ consumer_id+". Exception Details:" + ex.getMessage());
+			System.err.println(ex.getMessage());
+		}
+		return result;
+	}
+
 
 
 }
